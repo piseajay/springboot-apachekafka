c43bc6c0daa11532b55e0319abf2d70271f26d3c
Ajay M Pise <GS-2968@GSLAB.COM>
2021-07-26 22:26:12 +0530

    apachekakfa and spring boot integration changes

---
+++ src/main/java/com/amp/apachekafka/SpringBootWithApacheKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.amp.apachekafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithApacheKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithApacheKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amp/apachekafka/SpringBootWithApacheKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amp.apachekafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithApacheKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
